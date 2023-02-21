abf09074e1a2cbdb72019194ebf550422ce35a6e
marciodesenvolvedor <89565704+marciodesenvolvedor@users.noreply.github.com>
2023-02-21 20:08:49 -0300

    Adicionei arquivos via Upload

---
+++ Hello.GitHub/src/br/com/marciodesenvolvedor/git/HelloGit.java
@@ -0,0 +1,10 @@
+package br.com.marciodesenvolvedor.git;
+
+public class HelloGit {
+	/*Exemplo de integração do Eclipse com o GitHub*/
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
